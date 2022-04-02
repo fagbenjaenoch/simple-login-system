e5848b2d5342f59059cb12e1272fb1ba5c0660ee
Fagbenja Enoch <fagbenjaenoch73@gmail.com>
2022-04-02 12:59:50 +0100

    signup database file update

--- include/signup.php
+++ include/signup.php
@@ -1,5 +1,24 @@
 <?php
 
+$hostname = "localhost";
+$fullname = "";
+$email = "";
+$password = "";
+
+$dbconnect = mysql_connect($hostname,$fullname,$email,$password) || die ("Unable to connect to our servers");
+echo "Connected successfully";
+
+/* OR
+
+$dbselect = mysql_select_db('dbname',$dbconnect ) || die("Could not connect to dbname");
+
+* Another way to check if the database has been connected *
+if (!dbconnect){
+    die("Connection to the server failed: " . mysqli_connect_error())
+}
+else echo "Connected successfully";
+*/
+
 if (isset($_POST['fullname'])){
     echo 'Your nanme is'. $_POST['fullname'];
 }
