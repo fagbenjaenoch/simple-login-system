d067c3b14b9a3d5e5d396a4e7623f27d5f35833c
Fagbenja Enoch <fagbenjaenoch73@gmail.com>
2022-04-04 13:35:10 +0100

    Created main-header.php to hanadle the index.html.php header

---
+++ include/main-header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="logo">Mailly</div>
+        <nav>
+            <ul>
+                <li><a href="index.html.php">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact us</a></li>
+                <li><a href="login.html.php">Login</a></li>
+                <li><a href="signup.html.php" class="active">Signup</a></li>
+            </ul>
+        </nav>
+    </header>
